5184c4f4840f761c040ebcfc297302fc7b6b133f
亢哲 <kangzhe@weidian.com>
2016-11-17 10:01:43 +0800

    enhance function

--- ApkParser.py
+++ ApkParser.py
@@ -5,25 +5,36 @@ import sys
 import subprocess
 import threading
 import os
-import re
+import shutil 
 
 class Parser(object):
-	def __init__(self,apk_path):
-		self.apk_path = apk_path
+	def __init__(self,path):
+		self.current_path = self.current_path()
+		self.output = self.current_path + "/output"
+		self.apk_path = path[1]
+		self.dex_to_jar_path = path[2]
 		self.apk_name = self.parseName(self.apk_path)
-		self.parent_path = os.path.dirname(apk_path)
-		self.decompile_file_path = self.parent_path + "/" + self.apk_name
+		self.parent_path = os.path.dirname(self.apk_path)
 		self.process = None
 	
+	def current_path(self):
+		return os.path.normpath(os.path.join(os.getcwd(),os.path.dirname(__file__)))
+
 	def parseName(self,apk_path):
 		splittable = apk_path.split('/')
 		full_apk_name = splittable[len(splittable) - 1]
 		return full_apk_name.split('.')[0]
 
+	def copyApk(self):
+		shutil.move(self.apk_path, self.dex_to_jar_path)   
+
+	def changeEnv(self):
+		os.chdir(self.dex_to_jar_path)
+
 	def decompile(self):
 		print '.....decompile started.....'
 		def doDecompile():
-			apktool_cmd = "apktool d " + self.apk_path
+			apktool_cmd = "apktool d " + self.apk_name + ".apk"
 			self.process = subprocess.Popen(apktool_cmd, shell=True)
 			self.process.communicate()
 
@@ -35,7 +46,7 @@ class Parser(object):
 	def recompile(self):
 		print '.....recompile started.....'
 		def doRecompile():
-			apktool_cmd = "apktool b " + self.decompile_file_path
+			apktool_cmd = "apktool b " + self.apk_name
 			self.process = subprocess.Popen(apktool_cmd, shell=True)
 			self.process.communicate()
 
@@ -46,11 +57,11 @@ class Parser(object):
 
 	def dex_to_jar(self):
 		print '.....dex2jar started.....'
-		dest = self.decompile_file_path + "/build/apk/"
+		dest = self.apk_name + "/build/apk/"
 		for filename in os.listdir(dest):
 			if not filename.find("classes"): 
 				def doDex_to_jar():
-					cmd = "./d2j-dex2jar.sh " + self.decompile_file_path + "/build/apk/" + filename
+					cmd = "./d2j-dex2jar.sh " + self.apk_name + "/build/apk/" + filename
 					self.process = subprocess.Popen(cmd, shell=True)
 					self.process.communicate()
 
@@ -59,13 +70,26 @@ class Parser(object):
 				thread.join()
 		print '.....dex2jar finished.....'
 
+	def createDir(self):
+		os.mkdir(self.output)
+
+	def restoreFile(self):
+		shutil.move(self.apk_name, self.output)    
+		for dex in os.listdir(self.dex_to_jar_path):
+			if not dex.find("classes"): 
+				shutil.move(dex, self.output)    
+
 	def run(self):
+		self.copyApk()
+		self.changeEnv()
 		self.decompile()
 		self.recompile()
 		self.dex_to_jar()
+		self.createDir()
+		self.restoreFile()
 
 def parse():
-	parser = Parser(sys.argv[1])
+	parser = Parser(sys.argv)
 	parser.run()
 
 if __name__ == "__main__":
--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-##README will be presented when I finished this script.
+#Usage
+
+First,change the current path to the parent directory of ApkParser.py file
+
+Second,execute the ApkParser.py file in terminal like this:
+
+```
+python ./ApkParser.py /Users/kangzhe/python/test.apk /Users/kangzhe/dex2jar-2.0/
+```
+
+```
+/Users/kangzhe/python/test.apk is the path of the .apk file you want to decompile
+```
+
+```
+/Users/kangzhe/dex2jar-2.0/ is the path of dex2jar
+```
+
+Finally,you will see the result in output directory.
 
 
 
@@ -19,4 +37,3 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ```
-
